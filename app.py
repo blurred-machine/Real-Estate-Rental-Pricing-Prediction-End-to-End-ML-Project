6e564295204758f5734647ca0dc2e34dcf3007aa
Paras Varshney <paras.varshney97@gmail.com>
2020-05-25 18:26:15 +0530

    working on client side interface
    Signed-off-by: Paras Varshney <paras.varshney97@gmail.com>

--- app.py
+++ app.py
@@ -5,3 +5,41 @@ Created on Mon May 25 18:06:28 2020
 @author: paras
 """
 
+from flask import Flask, request
+import flask
+import joblib
+
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    form_data = request.form.to_dict()
+    print("FORM DATA")
+    print(form_data)
+    
+    df_input = pd.DataFrame.from_records([form_data])
+    df_input = pd.DataFrame(df_input)
+    print("INPUT DATAFRAME")
+    print(df_input)       
+    df_input = df_input.astype('int')
+    print(df_input.info())
+    
+    return flask.render_template('index.html', predicted_value="{}".format(str(pred_val)), any_message=msg)
+
+
+if __name__ == '__main__':
+    random_regressor = joblib.load("./pickles/random_regressor.pkl")
+    min_max_scaler = joblib.load("./pickles/min_max_scaler.pkl")
+    data_columns = joblib.load("./pickles/data_columns.pkl")
+    
+    app.run(host='0.0.0.0', port=8088)
+    
+    
