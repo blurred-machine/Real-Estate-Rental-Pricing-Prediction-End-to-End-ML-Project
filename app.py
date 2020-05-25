c789e28e733ec1163bacc2d0b7bbbae768fbbdb5
Paras Varshney <paras.varshney97@gmail.com>
2020-05-26 03:54:56 +0530

    minor prediction bugs solved

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def clean_data(raw_df):
     long_max_mask = raw_df['long'] <= -68.01197
     raw_df = raw_df[(long_min_mask) & (long_max_mask)]    
     raw_df.dropna(inplace=True)
-    print(raw_df)
     return raw_df
 
 
@@ -35,7 +34,7 @@ def feature_engineer_data(clean_df):
     try:
         clean_df = clean_df.drop(['id', 'url', 'region_url', 'image_url', 'description'], axis=1)
     except:
-        pass
+        print("Custom Error: drop columns did not execute!!")
     
     lat_long_pred = lat_long_classifier.predict(clean_df[["lat", "long"]])
         
@@ -45,6 +44,10 @@ def feature_engineer_data(clean_df):
     clean_df.fillna(-1)
     clean_df = pd.get_dummies(clean_df,drop_first=False)
     return clean_df
+
+def scale_data(df):
+    new_df = min_max_scaler.transform(df)
+    return new_df
 
 def prdict_results(df):
     random_regressor_pred = random_regressor.predict(df)
@@ -75,25 +78,30 @@ def predict():
     print('CLEANING DATA..............')
     clean_df = clean_data(df_input)
     print('FEATURING DATA.............')
-    df_new = feature_engineer_data(clean_df)
+    df_featured = feature_engineer_data(clean_df)  
+    
     
     print("DATA COLUMNS: //////////////")
     print(data_columns)
     sample_df = pd.DataFrame(columns = data_columns)
-    main_df = sample_df.append(df_new)
+    main_df = sample_df.append(df_featured)
     main_df = main_df.fillna(0)
     print("MAIN DATAFRAME: //////////////")
     print(main_df)
     print(main_df.info())
     print()
     
-    pred_val = "working"
+    print('SCALING DATA.............')
+    df_scaled = scale_data(main_df)
     
-    pred_val = prdict_results(main_df)
-    msg = f"Wohoo! AI predicts the price of this property to be around {pred_val[0]}$"
+    pred_val = ""
+    pred_val = round(prdict_results(df_scaled), 2)
+    print("PREDICTION: ////////////////")
+    print(pred_val)
+    msg = f"Wohoo! AI predicts the price of this property to be around {pred_val[0]} $"
 
     
-    return flask.render_template('index.html', predicted_value="{}".format("Prediction: "+str(pred_val[0])), any_message=msg)
+    return flask.render_template('index.html', predicted_value="{}".format("Prediction: "+str(pred_val[0])+" $"), any_message=msg)
 
 
 if __name__ == '__main__':
